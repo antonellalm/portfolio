0147cdb49da86a1ccfbac70308d7f3aa8033fee8
antonellalm <antonellarios0098@gmail.com>
2023-10-09 10:57:15 -0300

    first change o about

---
+++ components/about.tsx
@@ -0,0 +1,22 @@
+export const About = () => {
+  return (
+    <section>
+      <h2>About me</h2>
+      <p>
+        I have always been passionate about observing how software applications
+        and services have drastically changed people's habits, as well as how
+        they have influenced the lives of millions by providing them with
+        unimaginable services. I started studying marketing because I realized
+        that I wanted to enhance people's product and make it visible and
+        profitable for the public. Later, I pursued a diploma in Product
+        Management, which introduced me to the world of software products and
+        services. Upon completing the diploma, I understand of the endless
+        possibilities offered by software development. When I enrolled in a
+        coding bootcamp, I realized that it was my opportunity to learn web
+        development and contribute to the creation of products and services that
+        continue to impact people's lives.
+      </p>
+      <p></p>
+    </section>
+  );
+};
--- components/intro.tsx
+++ components/intro.tsx
@@ -5,11 +5,8 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { FaGithubSquare } from "react-icons/fa";
 import { HiDownload } from "react-icons/hi";
-import { useSectionInView } from "@/lib/hooks";
-import { useActiveSectionContext } from "@/context/active-section-context";
+
 export const Intro = () => {
-  const { ref } = useSectionInView("Home", 0.5);
-  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
   return (
     <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
@@ -71,17 +68,13 @@ export const Intro = () => {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection("Contact");
-            setTimeOfLastClick(Date.now());
-          }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
           href="/CV.pdf"
           download
         >
@@ -90,16 +83,16 @@ export const Intro = () => {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/antonella-rios-335503232/"
           target="_blank"
         >
           <BsLinkedin />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-110 active:scale-105 hover:text-gray-950 transition cursor-pointer border border-black/10"
+          href="https://github.com/antonellalm"
           target="_blank"
         >
           <FaGithubSquare />
