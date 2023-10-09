c5a547e743cb78cb5de82b6070f67e2e9f182175
antonellalm <antonellarios0098@gmail.com>
2023-10-09 15:10:34 -0300

    changes

--- components/about.tsx
+++ components/about.tsx
@@ -1,8 +1,8 @@
 export const About = () => {
   return (
-    <section>
-      <h2>About me</h2>
-      <p>
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+      <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+      <p className="mb-3">
         I have always been passionate about observing how software applications
         and services have drastically changed people's habits, as well as how
         they have influenced the lives of millions by providing them with
@@ -13,10 +13,18 @@ export const About = () => {
         services. Upon completing the diploma, I understand of the endless
         possibilities offered by software development. When I enrolled in a
         coding bootcamp, I realized that it was my opportunity to learn web
-        development and contribute to the creation of products and services that
+        development and contribute to the creation products and services that
         continue to impact people's lives.
       </p>
-      <p></p>
+      <p>
+        I am always looking to learn new technologies. I am currently looking
+        for a <span className="font-medium">full-time position</span> as a
+        software developer. My core stack is{" "}
+        <span className="font-semibold">
+          JavaScript, HTML, CSS, TypeScript, ReactJS, Redux, NextJS, Tailwind
+          CSS, NodeJS, Express.js, PostgreSQL, Sequelize
+        </span>{" "}
+      </p>
     </section>
   );
 };
