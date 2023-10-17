5f6f17b17e813df626742ceeef67c13723e06c04
antonellalm <antonellarios0098@gmail.com>
2023-10-17 10:50:33 -0300

    contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,17 +1,57 @@
+"use client";
 import React from "react";
 import { SectionHeading } from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
   return (
-    <section id="contact">
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="text-center mb-20 sm:mb-28 w-[min(100%,38rem)]"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+    >
       <SectionHeading>Contact me</SectionHeading>
-      <p>
+      <p className="text-gray-700">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:antonellarios0098@gmail.com">
           antonellarios0098@gmail.com
         </a>
         or through this form.
       </p>
-    </section>
+      <form action="mt-10" className="mt-10 flex flex-col">
+        <input
+          required
+          maxLength={500}
+          placeholder="Your email"
+          type="email"
+          className="h-14 px-4 rounded-lg borderBlack "
+        />
+        <textarea
+          required
+          maxLength={500}
+          placeholder="Your message"
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+        />
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 "
+        >
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />
+        </button>
+      </form>
+    </motion.section>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -12,6 +12,7 @@ import { useSectionInView } from "@/lib/hooks";
 
 export const Intro = () => {
   const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
@@ -78,6 +79,10 @@ export const Intro = () => {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
