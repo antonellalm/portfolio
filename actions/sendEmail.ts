ff87e6ab5477d441cfa2ec79ec21bd32e30ef0c8
antonellalm <antonellarios0098@gmail.com>
2023-11-02 19:47:21 -0300

    creation of utils forlder a it function

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,12 +1,8 @@
 "use server";
 import { Resend } from "resend";
+import { validateString } from "@/lib/utils";
+
 const resend = new Resend(process.env.RESEND_API_KEY);
-const validateString = (value: unknown, maxLenght: number) => {
-  if (!value || typeof value !== "string" || value.length > maxLenght) {
-    return false;
-  }
-  return true;
-};
 
 export const sendEmail = async (formData: FormData) => {
   // console.log("Running on server");
