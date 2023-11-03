46b6f6e398d760fcb3b0092162ce22bac9cfe7d0
antonellalm <antonellarios0098@gmail.com>
2023-11-03 12:46:41 -0300

    set up of react-email editing

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import {
+  Html,
+  Body,
+  Head,
+  Heading,
+  Hr,
+  Container,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
 
-export const ContactFormEmail = () => {
+type ContactFormEmailProps = {
+  message: string;
+};
+
+export const ContactFormEmail = ({ message }: ContactFormEmailProps) => {
   return <div>ContactFormEmail</div>;
 };
