cf57582269c817d618066931363ee0c20ed47b5e
antonellalm <antonellarios0098@gmail.com>
2023-11-03 20:18:39 -0300

    darkmood theme set up

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React, { useState } from "react";
-import { BsSun } from "react-icons/bs";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 type Theme = "light" | "dark";
 
@@ -13,8 +14,11 @@ export default function ThemeSwitch() {
     }
   };
   return (
-    <button className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.05rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all">
-      <BsSun />
+    <button
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.05rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
