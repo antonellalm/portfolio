07aed5311bd5038455157a467175a27ea47b85de
antonellalm <antonellarios0098@gmail.com>
2023-11-03 20:13:56 -0300

    finished footer

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSun } from "react-icons/bs";
 
+type Theme = "light" | "dark";
+
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<Theme>("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
     <button className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.05rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all">
       <BsSun />
