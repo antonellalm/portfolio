9397fd8149136503f9b89da9fabe45c0bffd78c9
antonellalm <antonellarios0098@gmail.com>
2023-11-04 16:49:41 -0300

    styling adjustment

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,7 +89,7 @@ export const Intro = () => {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10"
           href="/CV.pdf"
           download
         >
@@ -98,7 +98,7 @@ export const Intro = () => {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/antonella-rios-335503232/"
           target="_blank"
         >
@@ -106,7 +106,7 @@ export const Intro = () => {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-110 active:scale-105 hover:text-gray-950 transition cursor-pointer border border-black/10"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-110 active:scale-105 hover:text-gray-950 transition cursor-pointer border border-black/10 dark:bg-white/10"
           href="https://github.com/antonellalm"
           target="_blank"
         >
