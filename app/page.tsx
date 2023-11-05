ac56dfe2e5683d2b5a90898d054d33f78e563093
antonellalm <antonellarios0098@gmail.com>
2023-11-05 16:55:11 -0300

    cambios en page

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,16 @@ import { Intro } from "@/components/intro";
 import Projects from "@/components/projects";
 import { SectionDivider } from "@/components/section-divider";
 import Skills from "@/components/skills";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
   return (
     <main className=" flex flex-col items-center px-4">
-      <AnimatePresence mode="wait">
-        <Intro />
-        <SectionDivider />
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-      </AnimatePresence>
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </main>
   );
 }
